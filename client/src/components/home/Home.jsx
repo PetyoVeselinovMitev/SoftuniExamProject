a8700a496204e7d3535b22088897a0222c842129
PetyoVeselinovMitev <veselinovmitev@gmail.com>
2024-07-25 19:20:17 +0300

    implemented spinner in the home page

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -4,14 +4,17 @@ import './Home.css';
 
 import moviesAPI from '../../api/moviesApi';
 import RecentMovie from './recentMovie/RecentMovie';
+import Spinner from '../spinner/Spinner';
 
 export default function Home() {
     const [recentMovies, setRecentMovies] = useState([]);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         (async () => {
             const result = await moviesAPI.getAll();
             setRecentMovies(result.reverse().slice(0, 3));
+            setLoading(false);
         })()
     }, [])
 
@@ -19,12 +22,15 @@ export default function Home() {
         <div className="home">
             <h1>Recent Movies</h1>
             <div className="card-container">
-                {recentMovies.length > 0
-                    ? recentMovies.map(movie => <RecentMovie key={movie._id} {...movie} />)
-                    : <div className='msg'>
-                        <h2>There are no movies yet.</h2>
-                    </div>
-                }
+                {loading ? (
+                    <Spinner />
+                ) : (
+                    recentMovies.length > 0
+                        ? recentMovies.map(movie => <RecentMovie key={movie._id} {...movie} />)
+                        : <div className='msg'>
+                            <h2>There are no movies yet.</h2>
+                        </div>
+                )}
             </div>
 
         </div>
