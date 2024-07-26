2473be3239313fc78dc1b46b05462aeecbbc351f
PetyoVeselinovMitev <veselinovmitev@gmail.com>
2024-07-26 14:01:39 +0300

    removed password from state

--- client/src/hooks/useAuth.js
+++ client/src/hooks/useAuth.js
@@ -6,11 +6,11 @@ export const useUserLogin = () => {
     const {changeAuthState} = useContext(AuthContext);
 
     const loginHandler = async (email, password) => {
-        const result = await login(email, password);
+        const {password , ...authData} = await login(email, password);
         
-        changeAuthState(result);
+        changeAuthState(authData);
         
-        return result;
+        return authData;
     };
 
     return loginHandler;
