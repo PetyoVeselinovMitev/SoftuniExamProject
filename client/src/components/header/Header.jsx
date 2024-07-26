8133b6a00fc972103674a2c1968bfed297d75477
PetyoVeselinovMitev <veselinovmitev@gmail.com>
2024-07-26 14:37:19 +0300

    dinamicly update header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,27 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 
 import './header.css';
+import { AuthContext } from '../../contexts/AuthContext';
 
 const Header = () => {
+	const { isAuthenticated, name } = useContext(AuthContext);
 	return (
 		<header className="header">
 			<nav className="navbar">
 				<ul className="nav-links">
 					<li><Link to="/">Home</Link></li>
 					<li><Link to="/program">Program</Link></li>
-					<li><Link to="/login">Login</Link></li>
-					<li><Link to="/register">Register</Link></li>
+					{!isAuthenticated
+						&& (
+							<>
+								<li><Link to="/login">Login</Link></li>
+								<li><Link to="/register">Register</Link></li>
+							</>
+						)}
+
 				</ul>
-				<div className="user-section">
-					<span>Welcome, User!</span>
-					{/* if loged in user is the admin show this link */}
-					<li><Link to="/admin">Admin panel</Link></li>
-					{/* else show reservations */}
-					<li><Link to="/user/reservations">My reservations</Link></li>
-					{/* for loged in users */}
-					<li><Link to="/logout">Logout</Link></li>
-				</div>
+
+				{isAuthenticated
+					&& (<div className="user-section">
+						<span>Welcome, {name}!</span>
+						<li><Link to="/admin">Admin panel</Link></li>
+						<li><Link to="/user/reservations">My reservations</Link></li>
+						<li><Link to="/logout">Logout</Link></li>
+					</div>
+					)}
 			</nav>
 		</header>
 	);
