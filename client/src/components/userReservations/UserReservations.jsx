d429af49d48800ac8d7de9283d7a199479d05c2c
PetyoVeselinovMitev <veselinovmitev@gmail.com>
2024-08-05 02:14:50 +0300

    add heading in user reservations component

--- client/src/components/userReservations/UserReservations.jsx
+++ client/src/components/userReservations/UserReservations.jsx
@@ -9,27 +9,27 @@ export default function UserReservations() {
     const { userId } = useContext(AuthContext);
     const userReservations = useGetUserReservationsData(userId);
     return (
-        <div className="container">
-            {userReservations.length === 0
-                ? <div className='msg'>
-                    <h2>You don't have any reservations.</h2>
-                    <h3>Go to the <Link to="/program">Program</Link> to make a reservation.</h3>
-                </div>
-                : userReservations.map(reservation => {
-                    return (
-                        <div className="card" key={reservation._id}>
-                            <img src={reservation.movieImage} />
-                            <h3>{reservation.movieTittle}</h3>
-                            <p>Showtime: {reservation.showtime}</p>
-                            <p>Seats: {reservation.seats.join(', ')}</p>
-                            
-                        </div>
-                    )
-                })
-
-            }
-
-        </div>
+        <>
+            <h1>Your reservations.</h1>
+            <div className="container">
+                {userReservations.length === 0
+                    ? <div className='msg'>
+                        <h2>You don't have any reservations.</h2>
+                        <h3>Go to the <Link to="/program">Program</Link> to make a reservation.</h3>
+                    </div>
+                    : userReservations.map(reservation => {
+                        return (
+                            <div className="card" key={reservation._id}>
+                                <img src={reservation.movieImage} />
+                                <h3>{reservation.movieTittle}</h3>
+                                <p>Showtime: {reservation.showtime}</p>
+                                <p>Seats: {reservation.seats.join(', ')}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </>
     )
 }
 
