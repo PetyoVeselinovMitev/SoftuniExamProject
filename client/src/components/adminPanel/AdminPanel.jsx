00d91cf4f0fd3f6dd2e285d19f72f49f53d05a9f
PetyoVeselinovMitev <veselinovmitev@gmail.com>
2024-08-04 18:21:48 +0300

    fix style of admin panel

--- client/src/components/adminPanel/AdminPanel.jsx
+++ client/src/components/adminPanel/AdminPanel.jsx
@@ -9,41 +9,41 @@ import MovieRow from './movieRow/MovieRow';
 export default function AdminPanel() {
     const [loading, setLoading] = useState(true);
     const [movies] = useGetAllMoviesWithShowtimes(setLoading);
-   
+
 
     return (
-        <div className="movie-table-container">
-            {loading ? (
-                <Spinner />
-            ) : (
-                <>
-                    {movies.length === 0 && <h2 className='msg'>There are no movies yet.</h2>}
-                    <table className="movie-table">
-                        <thead>
-                            <tr>
-                                <th>Thumbnail</th>
-                                <th>Title</th>
-                                <th>Description</th>
-                                <th>Projection Times</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {movies.length > 0 && movies.map(movie =>
-                                <MovieRow
-                                    key={movie[0]._movieId}
-                                    movie={movie}
-                                />
-                            )}
-                        </tbody>
-                    </table>
-                    <div className="add-movie-btn">
-                        <Link to="/admin/create">Add Movie</Link>
-                    </div>
-                </>
-            )}
-
-
+        <div className="admin-panel-wrapper">
+            <div className="movie-table-container">
+                {loading ? (
+                    <Spinner />
+                ) : (
+                    <>
+                        {movies.length === 0 && <h2 className='msg'>There are no movies yet.</h2>}
+                        <table className="movie-table">
+                            <thead>
+                                <tr>
+                                    <th>Thumbnail</th>
+                                    <th>Title</th>
+                                    <th>Description</th>
+                                    <th>Projection Times</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {movies.length > 0 && movies.map(movie =>
+                                    <MovieRow
+                                        key={movie[0]._movieId}
+                                        movie={movie}
+                                    />
+                                )}
+                            </tbody>
+                        </table>
+                        <div className="add-movie-btn">
+                            <Link to="/admin/create">Add Movie</Link>
+                        </div>
+                    </>
+                )}
+            </div>
         </div>
     );
 }
